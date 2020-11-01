859a25f63d9bd8e4b07fb49cabdde0f889490282
A-Trash-Coder <gavynlamar@gmail.com>
2020-10-31 20:59:08 -0400

    First wave of mod commands

---
+++ cogs/staff_mod.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+from discord import message
+from discord.ext import commands
+import discord
+
+class Mod(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.mod_log = self.bot.main_guild.get_channel(716717997510885477)
+
+    async def do_case(self, ctx, member: discord.Member, reason, type):
+        last_case_number = await self.bot.mod_pool.fetchval("SELECT COUNT(*) FROM action")
+        embed = discord.Embed(title=type.title(), color=discord.Color.blurple(), description=f"""
+**__Victim:__** {member} ({member.id})
+**__Reason:__** ``{reason or f'No reason was set. Do b!reason {last_case_number + 1} <reason> to do so.'}``
+""")
+        embed.set_author(name=ctx.author, icon_url=(ctx.author.avatar_url))
+        embed.set_footer(text=f"Case #{last_case_number + 1}")
+        embed.timestamp = datetime.utcnow()
+        message = await self.mod_log.send(embed=embed)
+        await self.bot.mod_pool.execute("INSERT INTO action VALUES($1, $2, $3, $4, $5, $6)", member.id, ctx.author.id, reason or 'None', message.id, type, datetime.utcnow())
+        #userid, modid, reason, messageid, type, time, id
+
+    @commands.has_permissions(ban_members=True)
+    @commands.command()
+    async def ban(self, ctx, member: discord.Member, *, reason=None):
+        if member.bot:
+            return await ctx.send(f"You cannot ban bots! Use `b!delete @{member} <reason>` to do so.")
+
+        if not ctx.author.top_role > member.top_role:
+            return await ctx.send("You cannot ban someone higher than you!")
+
+        await self.do_case(ctx, member, reason, "Ban")
+
+    @commands.has_permissions(kick_members=True)
+    @commands.command()
+    async def kick(self, ctx, member: discord.Member, *, reason=None):
+        if member.bot:
+            return await ctx.send("You cannot kick bots! Use `b!delete @{member} <reason>` to do so.")
+
+        if not ctx.author.top_role > member.top_role:
+            return await ctx.send(f"You cannot kick someone higher than you!")
+
+        await self.do_case(ctx, member, reason, "Kick")
+
+    @commands.has_permissions(manage_messages=True)
+    @commands.command()
+    async def warn(self, ctx, member: discord.Member, *, reason=None):
+        if member.bot:
+            return await ctx.send("You cannot warn bots!")
+
+        if not ctx.author.top_role > member.top_role:
+            return await ctx.send(f"You cannot warn someone higher than you!")
+
+        await self.do_case(ctx, member, reason, "Warn")
+
+    @commands.command()
+    async def purge(self, ctx, amount: int):
+        if amount < 1:
+            return await ctx.send("Cannot purge that amount of messages!")
+        await ctx.channel.purge(limit=amount + 1)
+
+    @commands.command()
+    async def case(self, ctx, number: int):
+        info = await self.bot.mod_pool.fetch("SELECT * FROM action WHERE id = $1", number)
+        if info == []:
+            return await ctx.send(f"Case {number} does not exist!")
+        else:
+            info = info[0]
+
+        target = ctx.guild.get_member(info['userid'])
+        mod = ctx.guild.get_member(info['modid'])
+
+        embed = discord.Embed(title=info['type'].title(), color=discord.Color.blurple(), description=f"""
+**__Mod:__** {mod} ({mod.id})
+**__Victim:__** {target} ({target.id})
+**__Reason:__** ``{info['reason']}``
+**__Time:__** ``{info['time'].strftime('%c')}``
+""")
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def reason(self, ctx, number: int, *, reason):
+        info = await self.bot.mod_pool.fetch("SELECT * FROM action WHERE id = $1", number)
+        if info == []:
+            return await ctx.send(f"Case {number} does not exist!")
+        else:
+            info = info[0]
+
+        await ctx.send(f"Set the reason to: `{reason}`")
+        await self.bot.mod_pool.execute("UPDATE action SET reason = $1 WHERE id = $2", reason, number)
+        
+        target = ctx.guild.get_member(info['userid'])
+        mod = ctx.guild.get_member(info['modid'])
+
+        embed = discord.Embed(title=info['type'].title(), color=discord.Color.blurple(), description=f"""
+**__Victim:__** {target} ({target.id})
+**__Reason:__** ``{reason}``
+""")
+        embed.set_author(name=mod, icon_url=(mod.avatar_url))
+        embed.set_footer(text=f"Case #{number}")
+        embed.timestamp = info["time"]
+
+        message = await self.mod_log.fetch_message(info['messageid'])
+        await message.edit(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Mod(bot))
---
+++ cogs/staff_site.py
@@ -0,0 +1,204 @@
+import discord
+import googletrans
+from discord.ext import commands
+from . import checks  # pylint: disable=relative-beyond-top-level
+from typing import Union
+
+
+
+class Staff(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.has_permissions(kick_members = True)
+    @commands.group(invoke_without_command = True, aliases=["q"])
+    async def queue(self, ctx):
+        bots = await self.bot.pool.fetch("SELECT * FROM main_site_bot WHERE approved = False AND denied = False")
+        if not bots:
+            await ctx.send("There are no bots in the queue")
+            return
+
+        listed_bots = []
+        for x in bots:
+            invite = str(
+                discord.utils.oauth_url(x['id'], guild = self.bot.verification_guild)) + "&disable_guild_select=true"
+            listed_bots.append(f"{x['name']} [Invite]({invite})")
+
+        embed = discord.Embed(
+            title = "Queue",
+            description = '\n'.join(listed_bots) if listed_bots else "All Clear",
+            color = discord.Color.blurple()
+        )
+        await ctx.send(embed = embed)
+
+    @checks.main_guild_only()
+    @queue.command(aliases = ["c"])
+    async def certification(self, ctx):
+        bots = await self.bot.pool.fetch("SELECT * FROM main_site_bot WHERE awaiting_certification = True")
+        if bots is None:
+            await ctx.send("There are no bots in the certification queue")
+            return
+
+        listed_bots = []
+        for x in bots:
+            listed_bots.append(f"{x['name']} | Added: {x['joined']}")
+
+        embed = discord.Embed(
+            title = "Certification Queue",
+            description = '\n'.join(listed_bots) if listed_bots else "All Clear",
+            color = discord.Color.blurple()
+        )
+        await ctx.send(embed = embed)
+
+    @checks.verification_guild_only()
+    @commands.has_permissions(kick_members = True)
+    @commands.command()
+    async def approve(self, ctx, *, bot: discord.Member):
+        if not bot.bot:
+            await ctx.send("This is not a bot.")
+            return
+
+        bots = await self.bot.pool.fetchval(
+            "SELECT main_owner FROM main_site_bot WHERE approved = False AND denied = False AND id = $1", bot.id)
+        if not bots:
+            await ctx.send("This bot is not awaiting approval")
+            return
+
+        owner = self.bot.main_guild.get_member(bots)
+        if not owner:
+            await ctx.send(f"{ctx.author.mention}, The owner of this bot has left the main server, deny it!")
+            return
+
+        await self.bot.pool.execute("UPDATE main_site_user SET developer = True WHERE userid = $1", bots)
+        await self.bot.pool.execute("UPDATE main_site_bot SET approved = True WHERE id = $1", bot.id)
+
+        queued_bots = await self.bot.pool.fetchval(
+            "SELECT COUNT(*) FROM main_site_bot WHERE approved = False AND denied = False")
+        invite = str(discord.utils.oauth_url(bot.id, guild = self.bot.main_guild)) + "&disable_guild_select=true"
+        embed = discord.Embed(
+            title = f"Approved {bot.name}",
+            description = f"[Invite!]({invite})\n\nThere are {queued_bots} bot(s) in the queue.",
+            color = discord.Color.blurple()
+        )
+        await self.bot.verification_guild.get_channel(763183376311517215).send(content = ctx.author.mention, embed = embed)
+
+        em = discord.Embed(
+            description = f"``{bot.name}`` by ``{self.bot.main_guild.get_member(bots)}`` was approved by ``{ctx.author.name}``",
+            color = discord.Color.blurple())
+        await self.bot.get_channel(716446098859884625).send(embed = em)
+
+        dev_role = self.bot.main_guild.get_role(716684805286133840)
+
+        try:
+            await owner.send(f"Your bot `{bot}` was approved!")
+        except (discord.Forbidden, AttributeError):
+            pass
+
+        try:
+            await owner.add_roles(dev_role)
+        except AttributeError:
+            pass
+
+        await bot.kick()
+        bots = await self.bot.pool.fetchval("SELECT COUNT(*) FROM main_site_bot")
+        await self.bot.change_presence(activity = discord.Game(name = f"Watching {bots} bots"))
+
+    @commands.has_permissions(kick_members = True)
+    @commands.command()
+    async def deny(self, ctx, bot: Union[discord.Member, discord.User], *, reason):
+        if not bot.bot:
+            await ctx.send("This user is not a bot")
+            return
+
+        bots = await self.bot.pool.fetchval(
+            "SELECT main_owner FROM main_site_bot WHERE approved = False AND denied = False AND id = $1", bot.id)
+        if not bots:
+            await ctx.send("This bot is not awaiting approval")
+            return
+
+        try:
+            owner = self.bot.main_guild.get_member(bots)
+            await owner.send(f"Your bot `{bot}` was denied!")
+        except (discord.Forbidden, AttributeError):
+            pass
+
+        await self.bot.pool.execute("UPDATE main_site_bot SET denied = True WHERE id = $1", bot.id)
+        embed = discord.Embed(description = f"Denied {bot.name}", color = discord.Color.red())
+        await ctx.send(embed = embed)
+        em = discord.Embed(
+            description = f"``{bot.name}`` by ``{self.bot.main_guild.get_member(bots)}`` was denied by ``{ctx.author.name}`` for: \n```{reason}```",
+            color = discord.Color.red())
+        await self.bot.get_channel(716446098859884625).send(embed = em)
+        await bot.kick(reason = "Bot Denied")
+
+    @checks.main_guild_only()
+    @commands.has_permissions(kick_members = True)
+    @commands.command()
+    async def delete(self, ctx, bot: Union[discord.Member, int], *, reason):
+        bot_user = None
+        if isinstance(bot, discord.Member):
+            bot_user = bot
+        if isinstance(bot, int):
+            bot_user = self.bot.main_guild.get_member(bot)
+
+        if bot_user and not bot_user.bot:
+            await ctx.send("That is not a bot.")
+            return
+
+        bots = await self.bot.pool.fetchrow(
+            "SELECT main_owner, name, certified FROM main_site_bot WHERE approved = True AND id = $1", bot_user.id)
+        if not bots:
+            await ctx.send("This bot is not on the list")
+            return
+
+        await self.bot.pool.execute("DELETE FROM main_site_bot WHERE id = $1", bot_user.id)
+
+        embed = discord.Embed(description = f"Deleted {bots['name']}", color = discord.Color.red())
+        await ctx.send(embed = embed)
+
+        em = discord.Embed(
+            description = f"``{bots['name']}`` by ``{ctx.guild.get_member(bots['main_owner']) or bots['main_owner']}`` was deleted by ``{ctx.author.name}`` for: \n```{reason}```",
+            color = discord.Color.red())
+        await self.bot.get_channel(716446098859884625).send(embed = em)
+
+        member = ctx.guild.get_member(bots['main_owner'])
+        if bots['certified'] is True:
+            certified_dev_role = ctx.guild.get_role(716724317207003206)
+            await member.remove_roles(certified_dev_role)
+
+        has_other_bots = await self.bot.pool.fetch("SELECT * FROM main_site_bot WHERE main_owner = $1",
+                                                   bots['main_owner'])
+        if not has_other_bots and member:
+            dev_role = ctx.guild.get_role(716684805286133840)
+            await member.remove_roles(dev_role)
+            await self.bot.pool.execute("UPDATE main_site_user SET developer = False WHERE userid = $1",
+                                        bots['main_owner'])
+
+        if bot_user is not None:
+            await bot_user.kick(reason = "Bot Deleted")
+
+    @commands.has_permissions(kick_members = True)
+    @commands.command()
+    async def say(self, ctx, *, msg: commands.clean_content):
+        """Make blist repeat what you said"""
+        await ctx.send(msg)
+
+    @commands.has_permissions(kick_members = True)
+    @commands.command(hidden = True)
+    async def translate(self, ctx, to, *, message: commands.clean_content):
+        """Translates a message to English using Google translate."""
+        translator = googletrans.Translator()
+        try:
+            translated = translator.translate(message, dest = to)
+        except ValueError:
+            return await ctx.send("That is not a valid language")
+        src = googletrans.LANGUAGES.get(translated.src, '(auto-detected)').title()
+        dest = googletrans.LANGUAGES.get(translated.dest, 'Unknown').title()
+        embed = discord.Embed()
+        embed.add_field(name = f"{src} ({translated.src})", value = translated.origin.title(), inline = False)
+        embed.add_field(name = f"{dest} ({translated.dest})", value = translated.text.title(), inline = False)
+        await ctx.send(embed = embed)
+
+
+def setup(bot):
+    bot.add_cog(Staff(bot))
