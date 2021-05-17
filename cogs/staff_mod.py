a6e3ed36c257a728b732e44111e248c06799b78c
Mad <69681529+Mad880@users.noreply.github.com>
2021-05-17 16:57:23 +0430

    make the bot say something when you mute someone

--- cogs/staff_mod.py
+++ cogs/staff_mod.py
@@ -88,6 +88,7 @@ class Mod(commands.Cog):
         await member.add_roles(mute_role, reason=reason)
         case_number = await self.do_case(ctx.author, member, reason, "Mute", time=length)
         await self.bot.mod_pool.execute("INSERT INTO mutes VALUES($1, $2, $3, $4, $5)", ctx.author.id, member.id, datetime.datetime.utcnow(), length.dt, case_number)
+        await ctx.send(f"{member} was successfully muted for {length}\nReason: *{reason}*")
 
     @commands.has_permissions(kick_members=True)
     @commands.command()
